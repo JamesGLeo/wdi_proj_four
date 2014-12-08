9e624563ac63f9fd79e16a8fe93ebb2c836dd4c9
jamescpliu <jamesc.pliu@gmail.com>
2014-12-08 01:41:45 -0500

    csv's/db updated, models, controllers, and scopes added.

--- parking_app-railsapi/app/models/regulation.rb
+++ parking_app-railsapi/app/models/regulation.rb
@@ -1,2 +1,10 @@
 class Regulation < ActiveRecord::Base
+  belongs_to :location, :foreign_key => :sign
+
+  scope :day_parking_restricted, -> (day){ where("lower(description)LIKE ?", "%#{day.downcase}")}
+  scope :time_parking_restricted, -> (time){ where("lower(description)LIKE ?", "%#{time.downcase}")}
+
+  def as_json(options = nil)
+    super(options.merge(except: [:created_at, :updated_at, :regulation_number]))
+  end
 end
