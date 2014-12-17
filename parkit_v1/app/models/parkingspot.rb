18dae82fa6ba88553a2126d2d796b7d8a880f84d
thareefm <thareef.m@gmail.com>
2014-12-17 15:36:14 -0500

    pre filter

--- parkit_v1/app/models/parkingspot.rb
+++ parkit_v1/app/models/parkingspot.rb
@@ -1,17 +1,40 @@
 require 'geokit'
 
 class Parkingspot < ActiveRecord::Base
-  scope :sunday_no_rules, {where("sunday LIKE NULL"}
-  scope :monday_no_rules, {where("monday LIKE NULL"}
-  scope :tuesday_no_rules, {where("tuesday LIKE NULL"}
-  scope :wednesday_no_rules, {where("wednesday LIKE NULL"}
-  scope :thursday_no_rules, {where("thursday LIKE NULL"}
-  scope :friday_no_rules, {where("friday LIKE NULL"}
-  scope :saturday_no_rules, {where("saturday LIKE NULL"}
+  scope :sunday_no_rules, -> { where("sunday IS NULL")}
+  scope :monday_no_rules, -> {where("monday IS NULL")}
+  scope :tuesday_no_rules, -> {where("tuesday IS NULL")}
+  scope :wednesday_no_rules, -> {where("wednesday IS NULL")}
+  scope :thursday_no_rules, -> {where("thursday IS NULL")}
+  scope :friday_no_rules, -> {where("friday IS NULL")}
+  scope :saturday_no_rules, -> {where("saturday IS NULL")}
   acts_as_mappable :default_units => :kms,
                    :default_formula => :sphere,
                    :lat_column_name => :latitude,
                    :lng_column_name => :longitude
+
+
+
+  def self.closeby_no_rules(latitude, longitude, distance, day)
+    day.downcase!
+    case day
+      when "sunday"
+        spots_closeby = Parkingspot.sunday_no_rules.within(distance, :origin => [latitude, longitude]).all
+      when "monday"
+        spots_closeby = Parkingspot.monday_no_rules.within(distance, :origin => [latitude, longitude]).all
+      when "tuesday"
+        spots_closeby = Parkingspot.tuesday_no_rules.within(distance, :origin => [latitude, longitude]).all
+      when "wednesday"
+        spots_closeby = Parkingspot.wednesday_no_rules.within(distance, :origin => [latitude, longitude]).all
+      when "thursday"
+        spots_closeby = Parkingspot.thursday_no_rules.within(distance, :origin => [latitude, longitude]).all
+      when "friday"
+        spots_closeby = Parkingspot.friday_no_rules.within(distance, :origin => [latitude, longitude]).all
+      when "saturday"
+        spots_closeby = Parkingspot.saturday_no_rules.within(distance, :origin => [latitude, longitude]).all
+    end
+    return spots_closeby
+  end
 
 
   def self.close_to_me(latitude, longitude, distance, day)
