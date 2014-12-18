cbd4cd55868327127003354c3b1151b4a8dcc695
JamesCPLiu <jamesc.pliu@gmail.com>
2014-12-18 18:03:09 -0500

    cors added

--- parkit_v1/config/application.rb
+++ parkit_v1/config/application.rb
@@ -19,5 +19,13 @@ module ParkitV1
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins "*"
+          resource '/spots*', methods: [:get]
+      end
+    end
+
   end
 end
